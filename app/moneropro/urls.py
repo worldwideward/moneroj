adbfd36287df614c2ae5c64cb4e360e1db8f9127
w <ward@oblak.be>
2025-03-08 13:40:24 +0100

    remove unused import

--- app/moneropro/urls.py
+++ app/moneropro/urls.py
@@ -19,7 +19,6 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls import include
 from django.conf.urls.static import static
-from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
