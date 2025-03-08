87a9d2bc0b70de05ee75bc009e6f1b7199c1bb3d
w <ward@oblak.be>
2025-03-08 13:06:51 +0100

    fix linter issues

--- app/tasks/celery.py
+++ app/tasks/celery.py
@@ -1,12 +1,10 @@
+'''Celery worker'''
+
 import sys
 import os
-import asyncio
-import django
-import time
 import datetime
-import json
+import asyncio
 from django.conf import settings
-from redis import Redis
 from celery import Celery
 from tasks.data_sync import check_for_updates
 from tasks.data_sync import check_monero_available
@@ -18,57 +16,53 @@ from tasks.data_sync import daily_objects_updates
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moneropro.settings')
 
-class celery():
+class TaskWorker():
+    '''Celery worker configuration'''
 
     def __init__(self):
         self.host = settings.MONEROJ_REDIS_HOST
         self.agent = Celery('tasks', broker=f'redis://{self.host}/0', backend=f'redis://{self.host}')
-        return None
 
 def work():
+    '''Celery worker logic'''
 
-    agent = celery().agent
+    agent = TaskWorker().agent
 
     @agent.task
     async def todo_list():
+        '''Tasks the worker should perform each run'''
 
         message = check_monero_available()
         print(message)
-
-        ## Set checks
-        update_btc = False
-        update_socials = False
-        update_data = False
 
         ## Calculate time for this session
         date = datetime.date
         timedelta = datetime.timedelta
 
-        now = int(datetime.datetime.now().strftime("%H"))
         yesterday = datetime.datetime.strftime(date.today() - timedelta(1), '%Y-%m-%d')
         date_aux = datetime.datetime.strftime(date.today() - timedelta(2), '%Y-%m-%d')
 
         ### check monero updates
         result = check_for_updates(yesterday)
 
-        if result == True:
+        if result is True:
             print("Executing XMR updates..")
             await xmr_updates(yesterday, date_aux)
 
         ### check competitor updates
         result = check_competitors_for_updates(yesterday)
 
-        if result == True:
+        if result is True:
             print("Executing Competitor updates..")
             await competitors_updates(yesterday)
 
         result = check_daily_objects_for_updates(yesterday)
 
-        if result == True:
+        if result is True:
             print("Executing daily updates..")
             daily_objects_updates(yesterday)
 
-        print(f'executed all jobs', flush=True)
+        print('Executed all jobs', flush=True)
         return None
 
     asyncio.run(todo_list())
