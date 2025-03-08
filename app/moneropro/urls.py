1caa39ff7cff87d390a3f7a11fd072dc8e5e5368
w <ward@oblak.be>
2025-03-08 13:08:42 +0100

    fix linter issues

--- app/moneropro/urls.py
+++ app/moneropro/urls.py
@@ -19,9 +19,6 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls import include
 from django.conf.urls.static import static
-#from charts import views
-
-from django.conf import settings
 from django.views.static import serve
 
 urlpatterns = [
