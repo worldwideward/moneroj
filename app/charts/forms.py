805cd6a880de8460c1ebd940a07a190323a7e253
w <ward@oblak.be>
2025-03-09 12:05:20 +0100

    fix linter issues

--- app/charts/forms.py
+++ app/charts/forms.py
@@ -1,7 +1,13 @@
-from .models import *
+'''Forms module'''
+
 from django import forms
+from .models import Coin
+
 
 class CoinForm(forms.ModelForm):
-	class Meta:
-		model = Coin
-		fields = ['name', 'date', 'priceusd', 'pricebtc', 'inflation', 'transactions', 'hashrate', 'supply', 'fee', 'revenue', 'blocksize', 'difficulty']
+    '''Form to add cryptocurrency coins to the database'''
+
+    class Meta:
+        '''Form layout for adding cryptocurrency coins'''
+        model = Coin
+        fields = ['name', 'date', 'priceusd', 'pricebtc', 'inflation', 'transactions', 'hashrate', 'supply', 'fee', 'revenue', 'blocksize', 'difficulty']
