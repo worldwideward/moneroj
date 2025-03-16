e748181a2f22d2c668366db7cf15dbc876fac623
w <ward@oblak.be>
2025-03-16 15:02:14 +0100

    update admin page layout

--- app/charts/admin.py
+++ app/charts/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
-from .models import Coin
 
-admin.site.register(Coin)
+from .models import Coin
+from .models import Social
+from .models import DailyData
+
+@admin.register(Coin)
+class CoinAdmin(admin.ModelAdmin):
+    list_display = ["name", "date"]
+    list_filter = ["name", "date"]
+    list_per_page = 10
+    search_fields = ["name"]
+    ordering = ["date"]
+
+
+@admin.register(Social)
+class SocialAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(DailyData)
+class DailyDataAdmin(admin.ModelAdmin):
+    pass
