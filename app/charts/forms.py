921c14b06dbdd44bed31669cda199c32e0a07f95
w <ward@oblak.be>
2025-05-15 01:06:02 +0200

    remove dead code

--- app/charts/forms.py
+++ app/charts/forms.py
@@ -1,16 +1,7 @@
 '''Forms module'''
 
 from django import forms
-from .models import Coin
 
 class CsvImportForm(forms.Form):
 
     csv_file = forms.FileField()
-
-class CoinForm(forms.ModelForm):
-    '''Form to add cryptocurrency coins to the database'''
-
-    class Meta:
-        '''Form layout for adding cryptocurrency coins'''
-        model = Coin
-        fields = ['name', 'date', 'priceusd', 'pricebtc', 'inflation', 'transactions', 'hashrate', 'supply', 'fee', 'revenue', 'blocksize', 'difficulty']
