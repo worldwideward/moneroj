23dbc2f7196c5bc902e051abf78410657936af7c
w <ward@oblak.be>
2025-07-15 22:02:42 +0200

    add monerica as data source

--- app/charts/update_data/merchants.py
+++ app/charts/update_data/merchants.py
@@ -4,15 +4,29 @@ from datetime import datetime
 from django.core.exceptions import ObjectDoesNotExist
 
 from charts.api.merchants import BTCmapAPI
+from charts.api.merchants import MonericaSession
 from charts.models import Adoption
 
-MERCHANTS_API = BTCmapAPI()
+BTCMAP_API = BTCmapAPI()
+MONERICA_API = MonericaSession()
 
 NOW = date.today()
 
 def add_merchants_entry():
 
-    merchants_data = MERCHANTS_API.get_aggregated_merchants_data()
+    btcmap_data = BTCMAP_API.get_aggregated_merchants_data()
+
+    monerica_data = MONERICA_API.get_aggregated_merchants_data()
+
+    merchants_data = {
+            'accept_btc': btcmap_data['accept_btc'],
+            'accept_eth': btcmap_data['accept_eth'],
+            'accept_bch': btcmap_data['accept_bch'],
+            'accept_ltc': btcmap_data['accept_ltc'],
+            'accept_xrp': btcmap_data['accept_xrp'],
+            'accept_dash': btcmap_data['accept_dash'],
+            'accept_xmr': btcmap_data['accept_xmr'] + monerica_data['accept_xmr']
+            }
 
     print(merchants_data)
 
