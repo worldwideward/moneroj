e483f48421a463609de71ecf27e5d4dcc7dab46a
w <ward@oblak.be>
2025-07-15 16:24:46 +0200

    fix undefined exception

--- app/charts/update_data/subscribers.py
+++ app/charts/update_data/subscribers.py
@@ -1,6 +1,8 @@
 from charts.models import Dread
 from charts.api.tor import DreadSession
 
+from django.core.exceptions import ObjectDoesNotExist
+
 def add_dread_entry(date):
 
     session = DreadSession()
@@ -8,7 +10,7 @@ def add_dread_entry(date):
     try:
         Dread.objects.get(date=date)
         print("[INFO] Entry already exists, skipping")
-    except DoesNotExist:
+    except ObjectDoesNotExist:
         btc_subscribers = session.get_dread_subscriber_count("btc")
 
         if btc_subscribers is None:
@@ -25,5 +27,7 @@ def add_dread_entry(date):
         entry.bitcoin_subscriber_count = btc_subscribers
         entry.monero_subscriber_count = xmr_subscribers
         entry.save()
+    except Exception as error:
+        print(f'[INFO] An unknown error occurred: {error}')
 
     return True
